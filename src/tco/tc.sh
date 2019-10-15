f0404aa6b7cb5bae74ad4c95810260d7a8903f01
msw <mswl@ya.ru>
2019-10-15 10:14:32 +0300

    tc.sh: new scheme

--- src/tco/tc.sh
+++ src/tco/tc.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 mount_path="/mnt/tc/"
-containers_path="/media/$USER/msw-ssd/o/"
+containers_path="/media/$USER/msw-ssd/.o/"
+
+if [ "$#" -eq 2 ]; then
+	name=$2
+	file=$(ls $containers_path -1 | egrep -o "^$name.*" -m 1)
+	mount="$mount_path$name"
+	container="$containers_path$file"
+
+	mount_readwrite="$container --keyfiles= --protect-hidden=no $mount"
+	mount_readonly="$container --keyfiles= --protect-hidden=no -m readonly $mount"
+fi
 
 if [ "$#" -eq 0 ]
 then
@@ -12,14 +22,14 @@ else
 		truecrypt -l -v
 	elif [ "$1" == "m" ]
 	then
-		echo "mount [$2]"
-		sudo mkdir -p "$mount_path"$2
-		truecrypt "$containers_path"$2.tc -k "" --protect-hidden=no "$mount_path"$2
+		echo "mount [$name]"
+		sudo mkdir -p $mount
+		truecrypt $mount_readwrite
 	elif [ "$1" == "mr" ]
 	then
-		echo "mount [$2] read-only"
-		sudo mkdir -p "$mount_path"$2
-		truecrypt "$containers_path"$2.tc -k "" --protect-hidden=no -m readonly "$mount_path"$2
+		echo "mount [$name] read-only"
+		sudo mkdir -p $mount
+		truecrypt $mount_readonly
 	elif [ "$1" == "d" ]
 	then
 		if [ "$#" -eq 1 ]
@@ -27,8 +37,8 @@ else
 			echo "dismount all"
 			truecrypt -d
 		else
-			echo "dismount [$2]"
-			truecrypt "$containers_path"$2.tc -d
+			echo "dismount [$name]"
+			truecrypt $container -d
 		fi
 	fi
 fi
