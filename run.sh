350a0ada0a940e0ffd9ecb604a813ca689eb877e
msw <mswl@ya.ru>
2020-07-14 15:09:41 +0300

    remove Amazon

--- run.sh
+++ run.sh
@@ -292,7 +292,6 @@ function Clean {
 
         Exec 'sudo apt purge -y gnome-mahjongg' "remove game mahjongg"
         Exec 'sudo apt purge -y aisleriot' "remove game solitaire"
-        Exec 'sudo apt purge -y ubuntu-web-launchers' "remove Amazon"
         Exec 'sudo apt purge -y update-notifier' "remove Update Notifier"
         Exec 'sudo apt autoremove -y'
         Exec 'sudo snap remove gnome-calculator' "remove gnome-calculator"
