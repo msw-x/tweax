e16d2ed9be2f3283f4b4a78d2a0f8744de402af4
msw <mswl@ya.ru>
2022-05-15 22:13:13 +0300

    test

--- chroot.sh
+++ chroot.sh
@@ -8,7 +8,11 @@ mount -av
 
 apt install -y --reinstall grub-efi-amd64-signed linux-generic linux-headers-generic
 update-initramfs -c -k all
+
+read -p "Press enter to continue 2"
 grub-install $BootDev --no-nvram
+
+read -p "Press enter to continue 3"
 update-grub
 grub-probe -t device /boot/grub
 grub-probe -t fs_uuid /boot/grub
