559c1c98ec56f6f404763fcb93a27eba21a91f98
msw <mswl@ya.ru>
2022-06-09 12:06:30 +0300

    fix wdl

--- wdl.sh
+++ wdl.sh
@@ -12,7 +12,8 @@ function Exist {
 
 function Download {
     local url=$1
-    local name=$2
+    local name=$(basename $url)
+    url=$(dirname $url)
     if Exist $name; then
         local size=$(stat -c%s "$name")
         local suff="B"
@@ -26,59 +27,100 @@ function Download {
         fi
         echo "[ready]: $name [$size $suff]"
     else
-        echo "[download]: $name"
-        wget "$url/$name"
+        echo "[download]: $name => $url"
+        wget --timeout=240 "$url/$name"
     fi
 }
 
+function FinalUrl {
+    local url=$1
+    url=$(curl -LIs -o /dev/null -w %{url_effective} $url)
+    echo $url
+}
+
+function RootUrl {
+    local url=$1
+    url=$(echo $url | grep -P -o '.*\.\w+(?=/)')
+    echo $url
+}
+
+function GetUrl {
+    local url=$1
+    local exp=$2
+    local ret=$(wget --timeout=20 -qO - $url | grep -P -o '(?<=href=").*?(?=")' | grep $exp | head -n 1)
+    if ! echo $ret | grep -q -P '\.\w+/'; then
+        if [[ $ret == '/'* ]]; then
+            url=$(RootUrl $url)
+        else
+            ret="/$ret"
+        fi
+        local file=$(basename $url)
+        if [[ $file == *'.php' ]]; then
+            url=$(dirname $url)
+        fi
+        ret=$url$ret
+    fi
+    if [[ $ret == '//'* ]]; then
+        ret=$(echo $ret | grep -P -o '(?<=//).*')
+    fi
+    local file=$(basename -- "$ret")
+    local ext="${file##*.}"
+    if [[ $ext == $file ]]; then
+        ext=''  
+    fi
+    if [[ $ext == '' ]]; then
+        ret=$(FinalUrl $ret)
+    fi
+    echo $ret
+}
+
 function DownloadSysinternals {
-    local url='https://download.sysinternals.com/files'
-    Download $url "Autoruns.zip"
-    Download $url "ProcessExplorer.zip"
-    Download $url "ProcessMonitor.zip"
-    Download $url "TCPView.zip"
-    Download $url "RAMMap.zip"
+    local url='download.sysinternals.com/files/'
+    Download $url'Autoruns.zip'
+    Download $url'ProcessExplorer.zip'
+    Download $url'ProcessMonitor.zip'
+    Download $url'TCPView.zip'
+    Download $url'RAMMap.zip'
 }
 
 function Download7z {
-    local url="https://www.7-zip.org/a"
-    local file=$(wget -qO - https://www.7-zip.org/ | grep "x64.*Download" | grep -o "7z.*exe")
-    Download $url $file
+    local url=$(GetUrl '7-zip.org' 'x64')
+    Download $url
 }
 
 function DownloadGit {
-    local url=$(wget -qO - https://git-scm.com/download/win | awk '/64-bit Git for Windows Setup/{print $0}' | grep -o "https.*exe")
-    local file=$(basename $url)
-    url=$(dirname $url)
-    Download $url $file
+    local url=$(GetUrl 'git-scm.com/download/win' '64')
+    Download $url
 }
 
 function DownloadFar {
-    local url=$(wget -qO - https://www.farmanager.com/download.php | grep -P -o "href=\".*?\"" | grep -o "files.*x64.*msi")
-    local file=$(basename $url)
-    url="https://www.farmanager.com/"$(dirname $url)
-    Download $url $file
+    local url=$(GetUrl 'farmanager.com/download.php' 'x64.*msi')
+    Download $url
+}
+
+function DownloadVlc {
+    local url=$(GetUrl 'videolan.org' 'win64')
+    Download $url
 }
 
 function DownloadSublime {
-    local url=$(wget -qO - https://www.sublimetext.com/download | grep -P -o "href=\".*?\"" | grep -o "https.*x64.*exe")
-    local file=$(basename $url)
-    url=$(dirname $url)
-    Download $url $file
+    local url=$(GetUrl 'sublimetext.com/download_thanks?target=win-x64' 'x64.*exe')
+    Download $url
 }
 
 function DownloadSmartGit {
-    local url=$(wget -qO - https://www.syntevo.com/smartgit/download | grep -P -o "href=\".*?\"" | grep -o "/.*win.*zip")
-    local file=$(basename $url)
-    url="https://www.syntevo.com/"$(dirname $url)
-    Download $url $file
+    local url=$(GetUrl 'syntevo.com/smartgit/download' 'win')
+    Download $url
 }
 
 function DownloadWireshark {
-    local url=$(wget -qO - https://www.wireshark.org/download.html | grep -P -o "href=\".*?\"" | grep -o "https.*-win64.*exe" | head -n 1)
-    local file=$(basename $url)
-    url=$(dirname $url)
-    Download $url $file
+    local url=$(GetUrl 'wireshark.org/download/win64' 'win')
+    Download $url
+}
+
+function DownloadTelegram {
+    local url=$(GetUrl 'desktop.telegram.org' 'win64')
+    Download $url
 }
 
 function DownloadAll {
@@ -86,9 +128,11 @@ function DownloadAll {
     Download7z
     DownloadGit
     DownloadFar
+    DownloadVlc
     DownloadSublime
     DownloadSmartGit
     DownloadWireshark
+    DownloadTelegram
 }
 
 function Run {
