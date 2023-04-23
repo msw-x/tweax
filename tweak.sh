3dedc1c60832208add3de47bb9088dc1da169ec3
msw <mswl@ya.ru>
2023-04-23 18:45:53 +0300

    fix opencv

--- tweak.sh
+++ tweak.sh
@@ -622,7 +622,7 @@ function InstallOpencv {
             libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
             libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev
             gfortran openexr libatlas-base-dev python3-dev python3-numpy
-            libtbb2 libtbb-dev libdc1394-dev libopenexr-dev
+            libtbb-dev libdc1394-dev libopenexr-dev
             libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
         "
 
