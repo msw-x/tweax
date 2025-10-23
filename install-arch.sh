368ed85ba3165bea0510d7ece42b8eeecbade194
msw <mswl@ya.ru>
2025-10-23 19:00:27 +0300

    log

--- install-arch.sh
+++ install-arch.sh
@@ -476,7 +476,9 @@ function MakePartitions {
     if ! $reinstall; then
         mkfs.fat -F32 $payPartition -n $PayFsLabel
         mkfs.ext4 -F $isoPartition -L $IsoFsLabel
+        echo "!!!! $rootPartition"
         mkfs.btrfs -f $rootPartition --label $RootTrapFsLabel
+        echo "~~~~ $rootPartition"
     fi
 
     # You must use luks1 here - Currently, the latest grub does support opening a luks2 partition,
