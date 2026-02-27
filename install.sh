94f7f1341e6750c4f217d161b39ebb4eea7acf45
msw <mswl@ya.ru>
2026-02-27 11:13:38 +0300

    conf and tools

--- install.sh
+++ install.sh
@@ -1,120 +1,15 @@
 #!/bin/bash
 
-# -e option instructs bash to immediately exit if any command has a non-zero exit status
-# -u affects to unused variables
 set -eu
 
-BootKey=boot.key
-RootKey=root.key
-RootHeader=root.lks
-RootOffsetMiB=512
-
-EfiMiB=100
-BootMiB=4000
-IsoMiB=8000
-
-LvmRootGiB=160
-
-EfiFsLabel='x-usb-efi'
-IsoFsLabel='x-usb-iso'
-PayFsLabel='x-usb-pay'
-RootTrapFsLabel='x-data'
-
-BootLabel='BOOT'
-RootLabel='ROOT'
-
-BootFS='bootfs'
-RootFS='rootfs'
-LvmVG='lvm'
-LvmRoot='root'
-LvmExt='ext'
-
-Secrets='/etc/secret'
-
-MntExt='/mnt/ext'
-
-TimeZone='Europe/Moscow'
-
-Locales='en_US ru_RU'
-
-# Tools
+source ./conf.sh
+source ./tools.sh
 
 TmpDir='/tmp/install-'$(date +%s%N)
 PwdDir=$(pwd)
 SrcDir=${PwdDir}'/src/install'
 
 Target='/mnt'
-
-Bold='\033[1m'
-Red='\e[31m'
-Green='\e[32m'
-Yellow='\e[33m'
-Blue='\e[34m'
-Purple='\e[35m'
-Cyan='\e[36m'
-NC='\e[0m'
-
-Time() {
-    local time="$(date -d @$(($(date +%s)-$start)) +"%Mm %Ss")"
-    echo
-    echo -e "${Bold}${Blue}time: $time${NC}"
-}
-
-Fatal() {
-    msg=$*
-    Time
-    echo
-    echo -e "${Red}$msg${NC}"
-    exit 1
-}
-
-Ls() {
-    local dir=$1
-    echo
-    echo -e "${Bold}${Blue}$dir${NC}"
-    ls -1 $dir
-    echo
-}
-
-Cat() {
-    local file=$1
-    echo
-    echo -e "${Bold}${Blue}$file:${NC}"
-    cat $file
-    echo
-    echo -e "${Blue}==============================${NC}"
-}
-
-New() {
-    local file="$1"
-    local value="$2"
-    echo $value | tee $file > /dev/null
-}
-
-Add() {
-    local file="$1"
-    local value="$2"
-    echo $value | tee -a $file > /dev/null
-}
-
-Set() {
-    local file="$1"
-    local name="$2"
-    local value="$3"
-    local separator="${4:-|}"
-    # update
-    sed -i "s${separator}.*${name}=.*${separator}${name}=${value}${separator}" "$file"
-    # insert
-    grep -q "$name" $file || echo -e "\n$name=$value" | tee -a $file > /dev/null
-}
-
-Put() {
-    local file="$1"
-    local placeholder="$2"
-    local value="$3"
-    local separator="${4:-|}"
-    sed -i "s${separator}@${placeholder}${separator}${value}${separator}g" $file
-}
 
 Chroot() {
     arch-chroot $Target /bin/bash -c "$*"
---
+++ tools.sh
@@ -0,0 +1,72 @@
+timestamp=$(date +%s)
+
+Bold='\033[1m'
+Red='\e[31m'
+Green='\e[32m'
+Yellow='\e[33m'
+Blue='\e[34m'
+Purple='\e[35m'
+Cyan='\e[36m'
+NC='\e[0m'
+
+Time() {
+    local time="$(date -d @$(($(date +%s)-$timestamp)) +"%Mm %Ss")"
+    echo
+    echo -e "${Bold}${Blue}time: $time${NC}"
+}
+
+Fatal() {
+    msg=$*
+    Time
+    echo
+    echo -e "${Red}$msg${NC}"
+    exit 1
+}
+
+Ls() {
+    local dir=$1
+    echo
+    echo -e "${Bold}${Blue}$dir${NC}"
+    ls -1 $dir
+    echo
+}
+
+Cat() {
+    local file=$1
+    echo
+    echo -e "${Bold}${Blue}$file:${NC}"
+    cat $file
+    echo
+    echo -e "${Blue}==============================${NC}"
+}
+
+New() {
+    local file="$1"
+    local value="$2"
+    echo $value | tee $file > /dev/null
+}
+
+Add() {
+    local file="$1"
+    local value="$2"
+    echo $value | tee -a $file > /dev/null
+}
+
+Set() {
+    local file="$1"
+    local name="$2"
+    local value="$3"
+    local separator="${4:-|}"
+    # update
+    sed -i "s${separator}.*${name}=.*${separator}${name}=${value}${separator}" "$file"
+    # insert
+    grep -q "$name" $file || echo -e "\n$name=$value" | tee -a $file > /dev/null
+}
+
+Put() {
+    local file="$1"
+    local placeholder="$2"
+    local value="$3"
+    local separator="${4:-|}"
+    sed -i "s${separator}@${placeholder}${separator}${value}${separator}g" $file
+}
