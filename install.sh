60ca37d9abf0aca5dd43933f73b6bca329212d6c
msw <mswl@ya.ru>
2026-02-27 11:03:10 +0300

    fstab

--- install.sh
+++ install.sh
@@ -764,11 +764,6 @@ SetFstab() {
 
     case $DistroID in
         ubuntu)
-            #sudo sed -i '\|boot/efi|d' $fstab
-            #local efiUUID=$(PartitionUUID $efiPartition)
-            #echo "UUID=$efiUUID /boot/efi vfat umask=0077 0 1" | tee -a $fstab
-            #Cat $fstab
-
             local crypttab=$Target/etc/crypttab
             Add $crypttab "$BootFS UUID=$bootUUID $Secrets/$BootKey luks"
             Add $crypttab "$RootFS UUID=$rootUUID $Secrets/$RootKey luks,header=$Secrets/$RootHeader"
