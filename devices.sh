9cae1590a9534903a9316e56d240c5d0aab50e4a
msw <mswl@ya.ru>
2026-02-28 23:01:47 +0300

    trace

--- devices.sh
+++ devices.sh
@@ -257,6 +257,7 @@ CloseDevices() {
     umount "$Target/boot" &> /dev/null || :
     umount "$Target" &> /dev/null || :
 
+    echo "bootDev: $bootDev"
     umount '/dev/'${bootDev}* &> /dev/null || :
     umount '/dev/'${rootDev}* &> /dev/null || :
 
